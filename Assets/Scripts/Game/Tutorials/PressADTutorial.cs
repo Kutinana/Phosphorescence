1f88a9a643cdfb94024b93473bf276f824d33ec4
Kanade Yoisaki <kanade@yoisaki.com>
2025-05-14 15:47:28 +0900

    Tutorial for movement

---
+++ Assets/Scripts/Game/Tutorials/PressADTutorial.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using Kuchinashi.Utils.Progressable;
+using Phosphorescence.Narration;
+using QFramework;
+using UnityEngine;
+
+namespace Phosphorescence.Game.Tutorials
+{
+    public class PressADTutorial : MonoBehaviour
+    {
+        public Progressable spriteRendererProgressable;
+
+        private void Awake()
+        {
+            TypeEventSystem.Global.Register<OnStoryEndEvent>(e => {
+                if (e.plot && e.plot.Id == "0.1")
+                {
+                    StartCoroutine(StartTutorial());
+                }
+            }).UnRegisterWhenGameObjectDestroyed(gameObject);
+        }
+
+        private IEnumerator StartTutorial()
+        {
+            yield return spriteRendererProgressable.LinearTransition(0.2f, 0f);
+            var coroutine = spriteRendererProgressable.PingPong(1f, 0.5f, 1f);
+
+            yield return new WaitUntil(() => Vector2.Distance(transform.position, PlayerController.Instance.transform.position) > 4f);
+            StopCoroutine(coroutine);
+            yield return spriteRendererProgressable.InverseLinearTransition(0.2f, 0f);
+        }
+    }
+}
